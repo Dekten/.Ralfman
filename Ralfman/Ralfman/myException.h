865911c7e00b4a27120120a19c626cc8f45035cf
Dekten <dekten2010@yandex.ru>
2016-05-17 21:53:00 +0300

    Поломка

---
+++ Ralfman/Ralfman/myException.h
@@ -0,0 +1,25 @@
+//myException.h
+#pragma once
+#include <iostream>
+using namespace std;
+
+class myException
+{
+protected:
+	string what_;
+	string where_;
+	int	value_;
+public:
+	myException() {};
+	~myException() {};
+	string getWhat() { return what_; }
+	string getWhere() { return where_; }
+	int	getValue() { return value_; }
+};
+
+class DataError : public myException
+{
+public:
+	DataError(const string what, const string where, const int n) { what_ = what; where_ = where; value_ = n; }
+};
+
